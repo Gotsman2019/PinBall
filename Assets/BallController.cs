88a2a9ef4d4b96f5fff32228f138c6138470b115
後藤啓文 <info@3uindustry.co.jp>
2019-04-02 19:25:41 +0900

    20190402
    touchに苦労理解深める必要あり

---
+++ Assets/BallController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    private float visiblePosZ = -6.5f;
+    private GameObject gameoverText;
+    private GameObject scoreText;
+   
+    private int p;
+    public int score = 0;
+
+    int Point(int s)
+    {
+        int ss;
+        ss = s + 10;
+        return ss;
+    }
+    int ScoreAdd(int z)
+    {
+        int zz;
+        zz = z + score;
+        return zz;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.scoreText = GameObject.Find("Score");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+      this.scoreText.GetComponent<Text>().text ="得点　"+ score ;
+
+
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            p = Point(10);
+        }
+
+        else if (other.gameObject.tag == "LargeStartag")
+        {
+            p = Point(25);
+        }
+        else if (other.gameObject.tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            p = Point(100);
+        }
+        else
+        {
+            p = 0;
+        }
+
+
+        score = ScoreAdd(p);
+
+       //Debug.Log(score);
+    }
+}
---
+++ Assets/TouchController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    int width = Screen.width;
+    int height = Screen.height;
+
+    private HingeJoint myHingeJoint;
+    private float defaultAngle = 20;
+    private float flickAngle = -20;
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+        SetAngle(this.defaultAngle);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+     { 
+        Touch[] myTouchs = Input.touches;
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+
+
+            //タッチしたらフリッパー動かす
+            if (myTouchs[i].phase == TouchPhase.Began && tag == "LeftFripperTag" && myTouchs[i].position.x <= width / 2)
+            {
+                SetAngle(this.flickAngle);
+            }
+            if (myTouchs[i].phase == TouchPhase.Began && tag == "RightFripperTag" && myTouchs[i].position.x > width / 2)
+            {
+                SetAngle(this.flickAngle);
+            }
+            //タッチしたらフリッパ元に戻す
+            if (myTouchs[i].phase == TouchPhase.Ended && tag == "LeftFripperTag")
+            {
+                SetAngle(this.defaultAngle);
+            }
+            if (myTouchs[i].phase == TouchPhase.Ended && tag == "RightFripperTag")
+            {
+                SetAngle(this.defaultAngle);
+            }
+        }
+    }
+}
